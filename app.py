e54a2abc052c922c11d6f390614ff88db1ab75c4
danne135 <jodn35@gmail.com>
2024-05-09 23:54:24 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,92 +3,50 @@ from linebot import (LineBotApi, WebhookHandler)
 from linebot.exceptions import(InvalidSignatureError)
 from linebot.models import *
 from liffpy import (LineFrontendFramework as LIFF, ErrorResponse)
+import os
+
+#======這裡是呼叫的檔案內容=====
 from message import *
 from new import *
 from Function import *
-import os
+#======這裡是呼叫的檔案內容=====
 
-app = Flask(__name__,static_folder='static', template_folder='templates')
+app = Flask(__name__, static_folder='static', template_folder='templates')
 
+# 環境變數取得token和secret
 CHANNEL_ACCESS_TOKEN = os.getenv('co8ujSpXAemy3BjpjuSmOkEhUDnwnDmzCwsCtN/5httvZW+17I3ty4PN1LxwzN4Zku0ni0kQ+XK3VwKqkwGnAjwFRPY/ujvnzoOfnFU1dmFxLWdjmriP2+SmQ1I+O9jURfasevDW78dWfOIYBLZZ2QdB04t89/1O/w1cDnyilFU=
 ')
 CHANNEL_SECRET = os.getenv('f9c9c240b595cc03fe498a9f08a4aa32')
+
+# 確保環境變數存在
+if CHANNEL_ACCESS_TOKEN is None or CHANNEL_SECRET is None:
+    raise ValueError("The CHANNEL_ACCESS_TOKEN and CHANNEL_SECRET need to be set as environment variables.")
 
 liff_api = LIFF(CHANNEL_ACCESS_TOKEN)
 line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(CHANNEL_SECRET)
 
-target_LIFF_APP_number = 10
-print(target_LIFF_APP_number,now_LIFF_APP_number)
-if now_LIFF_APP_number < target_LIFF_APP_number:
-    for i in range(target_LIFF_APP_number - now_LIFF_APP_number):
-        liff_api.add(view_type="full",view_url="https://www.google.com")
-
 @app.route("/")
 def index():
-    return render_template("./liff.html")
+    return render_template("index.html")
 
-# 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
+    # 獲取 X-Line-Signature 頭部資訊
     signature = request.headers['X-Line-Signature']
-    # get request body as text
+    # 獲取請求內容
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-    # handle webhook body
+
+    # 處理webhook主體
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
     return 'OK'
 
+# 其他消息處理功能省略...
 
-# 處理訊息
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    msg = event.message.text
-    if '最新合作廠商' in msg:
-        message = imagemap_message()
-        line_bot_api.reply_message(event.reply_token, message)
-    elif '最新活動訊息' in msg:
-        message = buttons_message()
-        line_bot_api.reply_message(event.reply_token, message)
-    elif '註冊會員' in msg:
-        message = Confirm_Template()
-        line_bot_api.reply_message(event.reply_token, message)
-    elif '旋轉木馬' in msg:
-        message = Carousel_Template()
-        line_bot_api.reply_message(event.reply_token, message)
-    elif '圖片畫廊' in msg:
-        message = test()
-        line_bot_api.reply_message(event.reply_token, message)
-    elif '功能列表' in msg:
-        message = function_list()
-        line_bot_api.reply_message(event.reply_token, message)
-
-    else:
-        message = TextSendMessage(text=msg)
-        line_bot_api.reply_message(event.reply_token, message)
-
-@handler.add(PostbackEvent)
-def handle_message(event):
-    if event.postback.data == 'Maso的鑄鐵坊':
-        message = TextSendMessage(text="Maso的鑄鐵坊，目前施工中...")
-        line_bot_api.reply_message(event.reply_token, message)
-
-
-@handler.add(MemberJoinedEvent)
-def welcome(event):
-    uid = event.joined.members[0].user_id
-    gid = event.source.group_id
-    profile = line_bot_api.get_group_member_profile(gid, uid)
-    name = profile.display_name
-    message = TextSendMessage(text=f'{name}歡迎加入')
-    line_bot_api.reply_message(event.reply_token, message)
-        
-        
-import os
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.getenv('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
